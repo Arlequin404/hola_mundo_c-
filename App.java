9b03191ac5f3b92c87c4b7076826ad15e5a91c03
Alex Ortis <87336367+Arlequin404@users.noreply.github.com>
2024-11-10 17:20:25 -0500

    se cambio el puerto

--- App.java
+++ App.java
@@ -2,12 +2,14 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        // Usa el puerto asignado por Railway a través de la variable de entorno PORT
         port(getHerokuAssignedPort());
+
         get("/", (req, res) -> "Hola Mundo desde Java!");
     }
 
     static int getHerokuAssignedPort() {
-        String port = System.getenv("PORT");
-        return port != null ? Integer.parseInt(port) : 4567;
+        String port = System.getenv("PORT"); // Railway asigna el puerto aquí
+        return port != null ? Integer.parseInt(port) : 8080; // Usa 8080 como puerto por defecto si no se asigna
     }
 }
